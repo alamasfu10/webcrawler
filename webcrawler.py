bcf0a5bba041461b9f5a5042bd0d22303c07717b
Alvaro Lamas <alvaro.lamas10@gmail.com>
2017-07-17 01:10:42 +0200

    Limit kwargs and add docstring

--- webcrawler.py
+++ webcrawler.py
@@ -8,31 +8,51 @@ from datetime import datetime
 from slugify import slugify
 
 
-def parse_html(html, **kwargs):
-    is_wikipedia_page = kwargs.get('is_wikipedia_page')
+def parse_html(
+    html,
+    is_wikipedia_page=False,
+    content_container_class='',
+    image_container_class='',
+):
+    """Parse the given HTML to retrieve relevant content.
+
+    It is intended to retrieve data from Wikipedia articles without additional
+    configuration or kwargs.
+    To retrieve the data from generic pages, the content and image container
+    classes are required.
+
+    Args:
+    url (str): URL of the page whose data is going to be retrieved
+    is_wikipedia_page (bool): True if wikipedia HTML, False if not.
+    content_container_class (str): CSS Class of the HTML content container
+        element. Required to retrieve relevant data of generic pages.
+    image_container_class (str): CSS Class of the HTML image container element.
+        Required to retrieve relevant data of generic pages.
+    """
+
     parsed_html = BeautifulSoup(html, 'html.parser')
 
     headline = parsed_html.body.find('h1')
     paragraph = None
 
     if is_wikipedia_page:
-        # Parse Paragraph
         content_container = parsed_html.body.find(
             'div',
             attrs={'id': 'bodyContent'}
         )
+
+        # Wikipedia Articles have the first relevat paragraph
+        # after the markup for the table on the right.
+        # This table can also contain <p> tags, which are not
+        # the desired first paragraph.
         for p in content_container.findAll('p'):
             if not p.findParents('table'):
                 paragraph = p
                 break
 
-        # Parse Image
         infobox = parsed_html.body.find('table', attrs={'class': 'infobox'})
         image = infobox.find('img') if infobox else None
     else:
-        content_container_class = kwargs.get('content_container_class')
-        image_container_class = kwargs.get('image_container_class')
-
         if not all([
             content_container_class,
             image_container_class
@@ -58,22 +78,40 @@ def parse_html(html, **kwargs):
     }
 
 
-def crawl(url, **kwargs):
+def crawl(
+    url,
+    content_container_class='',
+    image_container_class=''
+):
+    """Crawl a URL, retrieve and persist the relevant data and return it.
+
+    It is intended to retrieve data from Wikipedia articles without additional
+    configuration or kwargs.
+    To retrieve the data from generic pages, the content and image container
+    classes are required.
+
+    Args:
+    url (str): URL of the page whose data is going to be retrieved
+    content_container_class (str): CSS Class of the HTML content container
+        element. Required to retrieve relevant data of generic pages.
+    image_container_class (str): CSS Class of the HTML image container element.
+        Required to retrieve relevant data of generic pages.
+    """
+
     response = requests.get(url)
     response.raise_for_status()
     is_wikipedia_page = re.compile(r'.*(wikipedia.org)').match(url) is not None
 
-    if is_wikipedia_page:
-        kwargs.update({
-            'is_wikipedia_page': is_wikipedia_page
-        })
-
-    data = parse_html(response.content, **kwargs)
+    data = parse_html(
+        response.content,
+        is_wikipedia_page=is_wikipedia_page,
+        content_container_class=content_container_class,
+        image_container_class=image_container_class
+    )
 
     if not data:
         return
 
-    # Persist Data
     if not os.path.exists('data'):
         os.makedirs('data')
 
@@ -82,7 +120,7 @@ def crawl(url, **kwargs):
         slugify(data.get('headline'))
     )
 
-    with open('data/{}'.format(filename), 'w') as file:  # noqa
+    with open('data/{}'.format(filename), 'w') as file:
         json.dump(data, file)
         file.close()
 
